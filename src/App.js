4046975f06e112a52697bda1d88548611db729f1
NateY98 <nate.yeow@gmail.com>
2021-04-12 14:47:38 -0400

    app.js done, scope approved

--- src/App.js
+++ src/App.js
@@ -1,59 +1,74 @@
 import './App.css';
 import firebase from "./firebase.js";
 import { useState, useEffect } from "react";
-
+import { BlackKey, WhiteKey } from "./Keyboard.js"
+const keybindings = [
+  "a",
+  "w",
+  "s",
+  "e",
+  "d",
+  "f",
+  "t",
+  "g",
+  "y",
+  "h",
+  "u",
+  "j",
+  "k",
+];
 function App() {
-
-  const [one, setOne] = useState("");
-
-  // console.log(storage);
-  // storage.getDownloadURL()
-  const note = document.getElementById('key');
-
-  const playMusic = () => {
-    note.play();
-  }
-
-  const pauseMusic = () => {
-    note.pause();
-  }
-
+  const [one, setOne] = useState([]);
   useEffect( () => {
-    // const stRef = firebase.storage().ref()
-    const storage = firebase.storage();
-    // const img = storage.root;
-  
-    const files = [ 'rex.mp3'];
-    files.map( filename => {
-      storage
-        .ref(`/${filename}`)
-        .getDownloadURL()
-        .then( url => {
-          console.log(url);
-          setOne(url);
-        })
-      
-    })
-  }, [])
-
-
+    async function fetchMyFirebase() {
+      const storage = firebase.storage();
+      const files = [
+        "C4.mp3",
+        "Csharp4.mp3",
+        "D4.mp3",
+        "Dsharp4.mp3",
+        "E4.mp3",
+        "F4.mp3",
+        "Fsharp4.mp3",
+        "G4.mp3",
+        "Gsharp4.mp3",
+        "A4.mp3",
+        "Asharp4.mp3",
+        "B4.mp3",
+        "C5.mp3"
+      ];
+      const filesUrls = [];
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const url = await storage
+        .ref(`/${file}`)
+        .getDownloadURL();
+        filesUrls.push({
+          key: file.slice(0, -4),
+          url: url
+        });
+      }
+      setOne(filesUrls);
+    };
+    fetchMyFirebase();
+  }, []);
+  // RETURN ==========================================
   return (
     <div className="App">
-      <h1>hello</h1>
-      
-      {
-        
-        <>
-          <audio controls src={one} id="key">
-          
-          </audio>
-
-          <button onClick={() => playMusic()}>button</button>
-          <button onClick={() => pauseMusic()}>button</button>
-        </>
-        }
+      <h1>Piano</h1>
+      <ul>
+        {one.map( (note, i) => {
+          return i===1 || i===3 || i===6 || i===8 || i===10 ?
+            <BlackKey keybinding={keybindings[i]}
+                      key={i}
+                      note={note} />
+          :
+            <WhiteKey keybinding={keybindings[i]}
+                      key={i} 
+                      note={note} />
+        })}
+      </ul>
     </div>
   );
 }
-
 export default App;
